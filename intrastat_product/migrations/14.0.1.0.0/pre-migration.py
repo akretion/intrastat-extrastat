76aed47446128c86ae47988a7cdf116f8af74723
Alexis de Lattre <alexis.delattre@akretion.com>
2024-09-09 19:34:10 +0200

    intrastat_product: improve migration script

--- intrastat_product/migrations/14.0.1.0.0/pre-migration.py
+++ intrastat_product/migrations/14.0.1.0.0/pre-migration.py
@@ -2,12 +2,14 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from openupgradelib import openupgrade
 
-def migrate(cr, version):
-    if not version:
-        return
 
-    cr.execute(
-        'ALTER TABLE "intrastat_product_declaration" RENAME "type" '
-        'TO "declaration_type"'
-    )
+@openupgrade.migrate()
+def migrate(env, version):
+    if openupgrade.column_exists(env.cr, "intrastat_product_declaration", "type"):
+        openupgrade.logged_query(
+            env.cr,
+            "ALTER TABLE intrastat_product_declaration RENAME type "
+            "TO declaration_type"
+        )
